9f31b4a6adf5ce20292785a1a0d51dd3daabc3ea
jlb2886 <55092716+jlb2886@users.noreply.github.com>
2020-11-23 12:56:23 -0500

    Styles
    Now styles are in their own file under src/styles/styles.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import {
 } from 'react-native';
 import {Colors, Typography} from './styles'
 import {mockdata} from "./data/mockdata";
+import {styles} from "./styles/styles.js"
 
 const App: () => React$Node = () => {
   return (
@@ -57,84 +58,5 @@ function SectionContainer (title, description, image, date, author){
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  root: {
-    backgroundColor: Colors.BLACK,
-    flex: 1,
-  },
-  scrollView: {
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-    paddingVertical: 20,
-    backgroundColor: Colors.SECONDARY,
-    borderRadius: 10,
-    marginHorizontal: 20,
-    marginVertical: 10,
-  },
-  sectionTitle: {
-    ...Typography.FONT_SEMI_BOLD,
-    fontSize: Typography.FONT_SIZE_32,
-    color: Colors.WHITE,
-  },
-  headerContainer: {
-    ...Typography.FONT_SEMI_BOLD,
-    fontSize: Typography.FONT_SIZE_32,
-    paddingHorizontal: 24,
-    paddingVertical: 20,
-    color: Colors.WHITE,
-    marginBottom: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.WHITE,
-    marginHorizontal: 20,
-  },
-  articleTitle: {
-    ...Typography.FONT_SEMI_BOLD,
-    fontSize: Typography.FONT_SIZE_32,
-    color: Colors.WHITE,
-    marginBottom: 15,
-  },
-  authorDateStyle: {
-    marginTop: 8,
-    ...Typography.FONT_REGULAR,
-    fontSize: Typography.FONT_SIZE_16,
-    color: Colors.GRAY_LIGHT,
-  },
-  replacerBox: {
-    marginTop: 20,
-    marginBottom: 20,
-    width: '100%',
-    height: 190,
-    backgroundColor: Colors.PRIMARY,
-    textAlign: 'center',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    ...Typography.FONT_REGULAR,
-    fontSize: Typography.FONT_SIZE_16,
-    color: Colors.GRAY_LIGHT,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  colorBold: {
-    fontWeight: '900',
-    color: 'green',
-  },
-  footer: {
-    color: Colors.GRAY_LIGHT,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
